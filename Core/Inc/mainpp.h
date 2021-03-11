39bdb800f38d4b6f6d91142b96bab4f43588d496
greekindo <greekindo1@gmail.com>
2021-03-11 15:29:41 +0700

    Project using STM32 for connecting to ROS

---
+++ Core/Inc/mainpp.h
@@ -0,0 +1,28 @@
+/*
+ * mainpp.h
+ *
+ *  Created on: Mar 9, 2021
+ *      Author: yogi
+ */
+
+#ifndef INC_MAINPP_H_
+#define INC_MAINPP_H_
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void setup();
+void loop();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
+
+
+#endif /* INC_MAINPP_H_ */
---
+++ Core/Src/mainpp.cpp
@@ -0,0 +1,49 @@
+/*
+ * mainpp.cpp
+ *
+ *  Created on: Mar 9, 2021
+ *      Author: yogi
+ */
+
+#include <mainpp.h>
+#include <ros.h>
+#include <std_msgs/Int32.h>
+
+ros::NodeHandle nh;
+
+std_msgs::Int32 num_msg;
+
+ros::Publisher num_pub("num",&num_msg);
+uint32_t nowTick = 0;
+uint32_t pastTick = 0;
+int num_test = 0;
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
+	//if (huart->Instance == USART1)
+		nh.getHardware() -> flush();
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+	//if (huart->Instance == USART1)
+		nh.getHardware() -> reset_rbuf();
+}
+
+void setup()
+{
+nh.initNode();
+nh.advertise(num_pub);
+
+
+}
+void loop()
+{
+nowTick = HAL_GetTick();
+if (nowTick - pastTick > 100)
+{
+	num_msg.data = num_test;
+	num_pub.publish(&num_msg);
+	nowTick = pastTick;
+	num_test++	;
+}
+}
+
